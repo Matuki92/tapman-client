f51618317327f79a5101f666107b5222a045a9bc
Matuki92 <matiaspierretti2@hotmail.com>
2018-07-25 14:47:21 +0200

    test 4

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class RestaurantService {
 
-  private baseUrl = `http://localhost:3000`;
+  private baseUrl = `http://dev.tapman.beer:3000`;
 
   constructor(private httpClient: HttpClient) { }
 
